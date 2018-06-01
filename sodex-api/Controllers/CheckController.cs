80e22aa97254a797363b39ab0523a48066e4fde5
Harold Glenn MinervaH <hgminerva@hotmail.com>
2018-05-31 20:43:17 -0700

    check api

---
+++ sodex-api/Controllers/CheckController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace sodex_api.Controllers
+{
+    public class CheckController : ApiController
+    {
+        [HttpGet, Route("Status")]
+        public String GetStatus()
+        {
+            return "Ok";
+        }
+    }
+}
